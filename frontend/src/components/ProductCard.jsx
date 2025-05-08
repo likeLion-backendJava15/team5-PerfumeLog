3722d4c1bd90cee295a8ae96d7065c7f192f888c
catapillar0505 <twrp3301@gmail.com>
2025-05-08 15:14:13 +0900

    feat: prodcutCard, productList 구현

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -3,15 +3,15 @@ import { Star } from 'lucide-react';
 
 const ProductCard = ({ imageUrl, brand, name, averageRating, reviewCount }) => {
   return (
-    <div className="border border-gray-200 rounded-lg p-4 flex gap-4 items-center">
-      <img src={imageUrl} alt={name} className="w-16 h-16 object-cover rounded-md" />
+    <div className="border border-gray-200 rounded-2xl p-4 flex gap-4 items-center shadow-sm">
+      <img src={imageUrl} alt={name} className="w-20 h-20 object-cover rounded-md" />
       <div className="flex-1">
         <p className="text-sm text-gray-500">{brand}</p>
-        <h3 className="text-lg font-semibold">{name}</h3>
-        <div className="flex items-center gap-1 text-yellow-500">
-          <Star size={16} fill='#F0C24D' />
+        <h3 className="text-base font-semibold">{name}</h3>
+        <div className="flex items-center gap-2 text-yellow-500 text-sm mt-1">
+          <Star size={16} fill="#F0C24D" strokeWidth={1} />
           <span>{averageRating}</span>
-          <span className="text-gray-400">리뷰({reviewCount})</span>
+          <span className="text-gray-400 ml-1">리뷰({reviewCount})</span>
         </div>
       </div>
     </div>
